3c647ea18b5652c2f7b7eea06f3ae42acad7462b
saifulu <saifulu@gmail.com>
2025-03-13 23:09:03 +0700

    Refactor app.jsx to use lowercase 'pages' in component resolution and remove unused app.tsx file

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,7 +7,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
     title: (title) => `${title} - Rumah Luka Caruban`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
         root.render(<App {...props} />);
